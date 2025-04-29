ade2df0543e6cd5e8c86c9062c363a02a1b4efd9
f0113800 <tiago.ventura@portoseguro.com.br>
2025-04-29 11:34:03 -0300

    feat: ajuste env vars

--- app/config.py
+++ app/config.py
@@ -1,15 +1,17 @@
+from pydantic import Field
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    prometheus_multiproc_dir: str | None = None
+    prometheus_multiproc_dir: str | None = Field(default=None, validate_default=False)
     app_name: str
-    logger_level: str = "WARNING"
-    endpoint_otel: str = "http://localhost:4317"
-    flag_local: bool = False
-    enable_tracing: bool = False
-    enable_metrics: bool = False
-    enable_logger: bool = False
-    enable_swagger: bool = False
+    logger_level: str = Field(default="WARNING", validate_default=False)
+    endpoint_otel: str = Field(default="http://localhost:4317", validate_default=False)
+    flag_local: bool = Field(default=False, validate_default=False)
+    enable_tracing: bool = Field(default=False, validate_default=False)
+    enable_metrics: bool = Field(default=False, validate_default=False)
+    enable_logger: bool = Field(default=False, validate_default=False)
+    enable_swagger: bool = Field(default=False, validate_default=False)
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
