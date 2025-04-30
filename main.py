297df7d12aeeffeed55712b01d121f5f4c67d32f
Tiago Ventura <tiago.barbano@gmail.com>
2025-04-29 21:01:48 -0300

    feat: ajuste main para importa quando flag True

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import app.core.tracing as otel  # noqa: F401
+import os
 
-from app.core.metrics import PrometheusMiddleware
-from app.core.logger import LoggerMiddleware
 from app.core.application import app
 from app.config import Settings
 from app.router import *  # noqa: F403
 
-
-from opentelemetry.instrumentation.asgi import OpenTelemetryMiddleware
-from opentelemetry.util.http import parse_excluded_urls
 
 settings = Settings()
 
@@ -16,16 +11,26 @@ settings = Settings()
 app = app
 
 # Configuração dos Middlewares
+
 # Habilita o Middleware de logger
 if settings.enable_logger:
+    from app.core.logger import LoggerMiddleware
+    
     app = LoggerMiddleware(app)
 
 # Habilita as Metricas do Prometheus
 if settings.enable_metrics:
+    from app.core.metrics import PrometheusMiddleware
+    
+    os.makedirs(settings.prometheus_multiproc_dir, exist_ok=True)
     app = PrometheusMiddleware(app)
 
 # Habilita o Tracing do OpenTelemetry
 if settings.enable_tracing:
+    import app.core.tracing as otel  # noqa: F401
+    from opentelemetry.instrumentation.asgi import OpenTelemetryMiddleware
+    from opentelemetry.util.http import parse_excluded_urls    
+    
     app = OpenTelemetryMiddleware(
         app,
         excluded_urls=parse_excluded_urls("/metrics,/openapi.json,/docs"),	
